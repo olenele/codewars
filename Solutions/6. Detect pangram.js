3d4140bcd9c11a31c341c83c9cc7f617b16d211f
olenele <elmo.mittila@hotmail.com>
2025-11-18 12:57:40 +0200

    Solution for Detect pangram

---
+++ Solutions/6. Detect pangram.js
@@ -0,0 +1,25 @@
+// kata: https://www.codewars.com/kata/545cedaa9943f7fe7b000048
+// 6 kyu
+
+/*
+A pangram is a sentence that contains every single letter of the alphabet at least once. 
+For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, 
+because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, 
+False if not. Ignore numbers and punctuation.
+*/
+
+
+function isPangram(string){
+  let alphabet = "abcdefghijklmnopqrstuvxyz";
+  string = string.toLowerCase();
+  
+  for (let i = 0; i < string.length; i++){
+    if (alphabet.includes(string[i])){
+      alphabet = alphabet.replace(string[i],"")
+    }
+  }
+ 
+  return alphabet.length === 0
+}
