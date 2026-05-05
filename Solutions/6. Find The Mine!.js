fa75de03b1a04f25022a4657b1e80b7c13d6e626
olenele <elmo.mittila@hotmail.com>
2026-05-05 12:05:41 +0300

    Solution for Find The Mine

---
+++ Solutions/6. Find The Mine!.js
@@ -0,0 +1,43 @@
+// kata: https://www.codewars.com/kata/528d9adf0e03778b9e00067e
+// 6 kyu
+
+/*
+Description:
+You've just discovered a square (NxN) field and you notice a warning sign. The sign states 
+that there's a single bomb in the 2D grid-like field in front of you.
+
+Write a function that accepts a 2D array, and returns the location of the mine. The mine 
+is represented as the integer 1 in the 2D array. Areas in the 2D array that are not the 
+mine will be represented as 0s.
+
+The location returned should be an array (Tuple<int, int> in C#, RAX:RDX in NASM, 
+std::pair<std::size_t, std::size_t> in C++) where the first element is the row index, 
+and the second element is the column index of the bomb location (both should be 0 based). 
+All 2D arrays passed into your function will be a square (NxN), and there will only be 
+one mine in the array.
+
+Examples (Input --> Output)
+[ [1, 0, 0], [0, 0, 0], [0, 0, 0] ] --> [0, 0]
+
+[ [0, 0, 0], [0, 1, 0], [0, 0, 0] ] --> [1, 1]
+
+[ [0, 0, 0], [0, 0, 0], [0, 1, 0] ] --> [2, 1]
+*/
+
+
+
+function mineLocation(field){
+  let minePosition = [];
+  let indexOfMine = -1;
+  
+  for (let i = 0; i < field.length; i++){
+    indexOfMine = field[i].findIndex(x => x === 1)
+    
+    if (indexOfMine !== -1){
+      minePosition = [i, indexOfMine];
+      break;
+    }
+  }
+  
+  return minePosition
+}
